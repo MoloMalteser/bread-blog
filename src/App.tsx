a2e9d336a87da95d93b57556611af3b3585cdd74
MoloMalteser <123650291+MoloMalteser@users.noreply.github.com>
2025-07-05 16:42:59 +0200

    App.tsx aktualisieren

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 import SecretAdminMenu from "./pages/secret-admin-menu"
+import Flappybread from "./pages/secret"
 
 const queryClient = new QueryClient();
 
@@ -47,6 +48,7 @@ const App = () => (
               <Route path="/terms" element={<Terms />} />
               <Route path="*" element={<NotFound />} />
               <Route path="/adminmenu" element={<SecretAdminMenu/>} />
+              <Route path="/game" element={<Flappybread/>} />
             </Routes>
           </BrowserRouter>
         </TooltipProvider>
